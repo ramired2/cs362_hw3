cbc4ea6bec657b0a7d97b18c0d243be7a4dc7f20
Daisy <ramired2@oregonstate.edu>
2021-04-23 21:25:27 -0700

    added error handling x>=0

---
+++ leapYear_errorHandling.py
@@ -0,0 +1,24 @@
+msg = "Please enter a year: "
+# year = input(msg)
+
+
+while True:
+    year = input(msg)
+
+    if int(year) >= 0:
+        if int(year) % 4 == 0:
+            
+            if int(year) % 100 == 0:
+                if int(year) % 400 == 0:
+                    print(year + " is a leap year")
+                    break
+                else:
+                    print(year + " is not a leap year")
+                    break
+            else:
+                print(year + " is a leap year")
+                break
+
+        else:
+            print(year + " is not a leap year")
+            break
