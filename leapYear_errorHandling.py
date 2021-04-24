b822ad15df4bb478fd32b6a5d0562ec9d0e05ce0
Daisy <ramired2@oregonstate.edu>
2021-04-23 21:55:53 -0700

    will not accept anything other than numeric values

--- leapYear_errorHandling.py
+++ leapYear_errorHandling.py
@@ -4,21 +4,32 @@ msg = "Please enter a year: "
 
 while True:
     year = input(msg)
+    
+    try:
+        yr = int(year)
+        # int(year) >= 0
+        # break
+    except ValueError:
+        print("That was not a valid input. Please enter a numeric value.\n")
+        continue
 
-    if int(year) >= 0 and len(year) >= 4:
-        if int(year) % 4 == 0:
-            
-            if int(year) % 100 == 0:
-                if int(year) % 400 == 0:
+    else:
+        if int(year) >= 0 and len(year) >= 4:
+            if int(year) % 4 == 0:
+                
+                if int(year) % 100 == 0:
+                    if int(year) % 400 == 0:
+                        print(year + " is a leap year")
+                        break
+                    else:
+                        print(year + " is not a leap year")
+                        break
+                else:
                     print(year + " is a leap year")
                     break
-                else:
-                    print(year + " is not a leap year")
-                    break
-            else:
-                print(year + " is a leap year")
-                break
 
+            else:
+                print(year + " is not a leap year")
+                break
         else:
-            print(year + " is not a leap year")
-            break
+            print("That was not a valid input. A year must be a positive number of at least four values.\n")
